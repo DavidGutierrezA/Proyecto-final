7a29fc514a1f4279497d0fe15647c5f224c56953
Ubuntu <ubuntu@ip-172-31-7-80.us-east-2.compute.internal>
2023-11-16 22:29:54 +0000

    cambios para funcionar con instancias

--- AppBack/app.js
+++ AppBack/app.js
@@ -12,7 +12,7 @@ import cors from 'cors'
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.use(cors())
 app.use(express.json());
 app.use('/api', userRoutes);
